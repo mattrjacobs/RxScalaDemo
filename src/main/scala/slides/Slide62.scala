d5360416099073cd2fe2d2a0f735dad9a7fe0c58
Matt Jacobs <mattrjacobs@gmail.com>
2013-10-05 10:17:58 -0700

    Added Slide62

---
+++ src/main/scala/slides/Slide62.scala
@@ -0,0 +1,30 @@
+package com.mattrjacobs.rxscalademo
+
+import java.util.concurrent.Executor
+import rx.subscriptions.Subscriptions
+import rx.lang.scala.{ Observable, Observer }
+
+trait Slide62 extends App {
+
+  case class VideoRating(userId: Long, videoId: Long, rating: Double)
+
+  val executor: Executor
+  def getRatingFromNetwork(userId: Long, videoId: Long): VideoRating
+
+  def getRating(userId: Long, videoId: Long): Observable[VideoRating] =
+    Observable(observer => {
+      executor.execute(new Runnable {
+        override def run() {
+          try {
+            val rating = getRatingFromNetwork(userId, videoId)
+            observer.onNext(rating)
+            observer.onCompleted()
+          } catch {
+            case ex: Throwable => observer.onError(ex)
+          }
+        }
+      })
+      Subscriptions.empty
+    })
+}
+
