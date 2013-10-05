e708b06ed3a52cb726835cc4cb727068064cb1d2
Matt Jacobs <mattrjacobs@gmail.com>
2013-10-05 12:04:31 -0700

    Added Slide141

---
+++ src/main/scala/slides/Slide141.scala
@@ -0,0 +1,37 @@
+package com.mattrjacobs.rxscalademo
+
+import rx.lang.scala.Observable
+
+trait Slide141 extends App {
+
+  trait VideoService {
+    trait Video {
+      val id: Long
+      def getMetadata(): Observable[Map[String, Object]]
+    }
+
+    def getVideos(userId: Long): Observable[Video]
+    def getBookmark(video: Video, userId: Long): Observable[Map[String, Object]]
+    def getRating(video: Video, userId: Long): Observable[Map[String, Object]]
+  }
+
+  val videoService: VideoService
+
+  def getVideos(userId: Long): Observable[Map[String, Any]] =
+    videoService.getVideos(userId)
+      .take(10)
+      .flatMap(video => {
+        val metadata = video.getMetadata.map(md =>
+          Map("title" -> md.get("title"),
+            "length" -> md.get("duration")))
+        val bookmark = videoService.getBookmark(video, userId).map(b =>
+          Map("position" -> b.get("position")))
+        val rating = videoService.getRating(video, userId).map(r =>
+          Map("rating" -> r.get("rating")))
+        Observable.zip(List(metadata, bookmark, rating)).map {
+          case m :: b :: r :: Nil =>
+            Map("id" -> video.id) ++ m ++ b ++ r
+        }
+      })
+}
+
