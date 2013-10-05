ce9adea5ff7258a98e56af1c50fd0777b2a3759d
Matt Jacobs <mattrjacobs@gmail.com>
2013-10-05 10:54:35 -0700

    Added Slide69

---
+++ src/main/scala/slides/Slide69.scala
@@ -0,0 +1,24 @@
+package com.mattrjacobs.rxscalademo
+
+import rx.subscriptions.Subscriptions
+import rx.lang.scala.{ Observable, Observer }
+
+trait Slide69 extends App {
+  trait Video {
+    val videoId: Long
+  }
+
+  def getVideos(): Observable[Video]
+
+  val subscription = getVideos.subscribe(new rx.Observer[Video] {
+    def onNext(v: Video) = println("Video: " + v.videoId)
+    def onError(ex: Throwable) {
+      println("Error")
+      ex.printStackTrace
+    }
+    def onCompleted() {
+      println("Completed")
+    }
+  })
+}
+
