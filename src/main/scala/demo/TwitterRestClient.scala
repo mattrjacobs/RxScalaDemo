e9d49339cbcb6f3a7b78f26c3b9d99ec350c505b
Matt Jacobs <mattrjacobs@gmail.com>
2013-10-18 09:54:10 -0700

    Removing hardcoded OAuth params

--- src/main/scala/demo/TwitterRestClient.scala
+++ src/main/scala/demo/TwitterRestClient.scala
@@ -4,11 +4,6 @@ import java.util.concurrent.CountDownLatch
 import rx.apache.http._
 import rx.lang.scala.Observable
 
-/*
- * Example curl
- * curl --get 'https://api.twitter.com/1.1/statuses/mentions_timeline.json' --data 'count=2&since_id=14927799' --header 'Authorization: OAuth oauth_consumer_key="ix3M2MWX1ircNSudKLpVcw", oauth_nonce="57e9bbd91259f7e7513a08dbe3177446", oauth_signature="sh3958IndFJrtGmd7c9kiDlS%2Fy0%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1381525038", oauth_token="6463862-NLQ9vwfqZBjX5hyVzDwzktYLeICfdknLIr1yutjbUg", oauth_version="1.0"' --verbose
-*/
-
 object TwitterRestClient extends TwitterClient {
 
   val latch = new CountDownLatch(1)
@@ -18,7 +13,11 @@ object TwitterRestClient extends TwitterClient {
 
     try {
       val response: Observable[ObservableHttpResponse] =
-        getResponse(TWITTER_USER_MENTIONS_REST_URI, "OAuth oauth_consumer_key=\"ix3M2MWX1ircNSudKLpVcw\", oauth_nonce=\"e8965178c1e927af4380fab3bc227a96\", oauth_signature=\"nuh7sKJ66yumgLZHy45ulqc0Zac%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1381928205\", oauth_token=\"6463862-NLQ9vwfqZBjX5hyVzDwzktYLeICfdknLIr1yutjbUg\", oauth_version=\"1.0\"")
+        //Very poor man's OAuth integration.  I set up a Twitter app and got
+        //OAuth header from: https://dev.twitter.com/docs/api/1.1/get/statuses/mentions_timeline.
+        //Remember to escape your quotes!
+        getResponse(TWITTER_USER_MENTIONS_REST_URI, "fill_this_in_with_oauth_header")
+      
 
       val mentionObs: Observable[Mention] = for {
         httpResp <- response
--- src/main/scala/demo/TwitterStreamingClient.scala
+++ src/main/scala/demo/TwitterStreamingClient.scala
@@ -12,7 +12,10 @@ object TwitterStreamingClient extends TwitterClient {
 
     try {
       val response: Observable[ObservableHttpResponse] =
-        getResponse(TWITTER_USER_STREAM_URI, "OAuth oauth_consumer_key=\"ix3M2MWX1ircNSudKLpVcw\", oauth_nonce=\"9875fd624a61f431c5a756f8d7fd1c71\", oauth_signature=\"GjkjuV8ycqKu4WqpB%2FmbrZTp1zc%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1381979943\", oauth_token=\"6463862-NLQ9vwfqZBjX5hyVzDwzktYLeICfdknLIr1yutjbUg\", oauth_version=\"1.0\"")
+        //Very poor man's OAuth integration. I set up a Twitter app and got
+        //OAuth header from: https://dev.twitter.com/docs/streaming-apis/streams/user.
+        //Remember to escape your quotes!
+        getResponse(TWITTER_USER_STREAM_URI, "fill_this_in_with_oauth_header")
 
       val mentionObs: Observable[Mention] = for {
         httpResp <- response
